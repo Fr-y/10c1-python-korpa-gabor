30639b0c7afaece7ceec40c0f814197d7ae7162b
Korpa Gábor Máté <korpa.gabor@sulinet.csmszki>
2022-03-03 09:38:02 +0100

    .

--- 2022.03.03/fajlbeolvasas.py
+++ 2022.03.03/fajlbeolvasas.py
@@ -4,26 +4,68 @@ def létezikE(filenév):
         f.close()
         return 1
     except:
+        print('A fájl nem létezik')
         return 0
+# --------------------------------------------------------
+
+def primek12ig():
+    # egy sor egy adat
+    filename = "primek12ig.txt"
+    if létezikE(filename):
+        f = open(filename, 'r')
+        primek = f.readlines()
+        f.close()
+        print(primek)
+        for i in primek:
+            i = i.strip('\n')
+            print(i, end=" ")
 
 
+def nemek():
+    # egy sor tobb adat
+    filename = "nemek.txt"
+    if létezikE(filename):
+        f = open(filename, 'r')
+        beolvas = f.readline()
+        f.close()
+        nemek = beolvas[:-1].split(',')
+        print(f'Az elemek száma: {len(nemek)} \nFérfiak száma: {nemek.count("f")} \nNők száma: {nemek.count("n")}  ')
+
+# cipomeret,veletlenek,sorozat,negyzetek
 
 
-    if létezikE(filenév):
-        utasítások
-    else:
-        print(’A fájl nem létezik’)
+def cipomeret():
+    # egy sor tobb adat
+    filename = "cipomeret.txt"
+    if létezikE(filename):
+        f = open(filename, 'r')
+        beolvas = f.readline()
+        f.close()
+        meretek = beolvas[:-1].split(' ')
+        ossz = 0
+        for meret in meretek:
+            ossz += int(meret)
+        atlag = ossz / len(meretek)
+        print(f'Cipőméretek átlaga: {atlag} \nLegnagyobb cipőméret: {max(meretek)} \nLegkisebb cipőméret: {min(meretek)}')
 
+def veletlenek():
+    pass 
 
+def sorozat():
+    pass
 
-
-
-
-
-
-
-
+def negyzetek():
+    pass
 
 
 
 # --main--
+# primek12ig()
+# nemek()
+# cipomeret()
+veletlenek()
+# sorozat()
+# negyzetek()
+
+
+
